a3bb3b999f2e94d2f420ed199a8ee1ce166f1471
Jorge Carabias <jorge@blarlo.com>
2024-11-09 09:59:13 +0100

    Added next config needed instead of configura-pages step in actions

--- next.config.mjs
+++ next.config.mjs
@@ -5,7 +5,13 @@ const withNextra = nextra({
   themeConfig: './theme.config.jsx'
 })
  
-export default withNextra()
+export default withNextra({
+  output: 'export',
+  images: {
+    unoptimized: true
+  },
+  basePath: "/pensero-gh-pages-template"
+})
  
 // If you have other Next.js configurations, you can pass them as the parameter:
 // export default withNextra({ /* other next.js config */ })
